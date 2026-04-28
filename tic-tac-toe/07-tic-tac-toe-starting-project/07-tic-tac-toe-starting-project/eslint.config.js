e0fa0064fe2bab2014e074c436644e0e896782ba
BursacAleksandar99 <aleksandarbursac150699@gmail.com>
2026-04-28 11:48:33 +0200

    Section 4. lesson 82

---
+++ tic-tac-toe/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "off",
+    },
+  },
+];
