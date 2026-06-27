a103ee9466ccfebf696eb75324472c6e1ffabbb3
BursacAleksandar99 <aleksandarbursac150699@gmail.com>
2026-06-27 13:47:19 +0200

    93. lesson, gameOver component added, we now how and draw case!

--- tic-tac-toe/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ tic-tac-toe/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -52,6 +53,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -78,7 +81,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>{" "}
-        {winner && <p>You win, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
--- tic-tac-toe/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ tic-tac-toe/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,19 +1,6 @@
 import { useState } from "react";
 
 export default function GameBoard({ onSelectSquare, board }) {
-  // const [gameBoard, setGameBoard] = useState(intialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSquare();
-  // }
-
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => (
